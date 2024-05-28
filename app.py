5f3d8912d0940b88582704f5543742c83da178d1
Amandus Krantz <krantz.amandus@protonmail.com>
2024-05-28 15:14:11 +0200

    replaced crazyswarm code with own interface

--- app.py
+++ app.py
@@ -22,12 +22,14 @@ cf = CrazyflieController({drone_uri}, flight_zone, drone_uri)
 
 goal_reached = False
 
+
 class Destination:
     def __init__(self, name, easy_location, hard_location, default_location):
         self.name = name
         self.easy_location = easy_location
         self.hard_location = hard_location
         self.default_location = default_location
+
 
 # Destinations have a name, easy_location, hard_location
 destinations = [
