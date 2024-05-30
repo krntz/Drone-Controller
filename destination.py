96244b9f978dcc63587bf1cff586676d050ce198
Amandus Krantz <krantz.amandus@protonmail.com>
2024-05-30 13:18:41 +0200

    removed junk files

--- destination.py
+++
@@ -1,8 +0,0 @@
-# destination.py
-class Destination:
-    def __init__(self, name, easy_location, hard_location, default_location):
-        self.name = name
-        self.easy_location = easy_location
-        self.hard_location = hard_location
-        self.default_location = default_location
-
